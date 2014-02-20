18b36983886d25c35e183926adf89de13b6795f0
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2014-02-20 02:14:18 +0100

    added missing copyright info

--- test/algorithm_test/copy_backward.cpp
+++ test/algorithm_test/copy_backward.cpp
@@ -1,9 +1,11 @@
 // Boost.Range library
 //
-//  Copyright Neil Groves 2009. Use, modification and
-//  distribution is subject to the Boost Software License, Version
-//  1.0. (See accompanying file LICENSE_1_0.txt or copy at
-//  http://www.boost.org/LICENSE_1_0.txt)
+//  Copyright Neil Groves 2009.
+//  Copyright Adam Wulkiewicz 2014.
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 //
 //
 // For more information, see http://www.boost.org/libs/range/
