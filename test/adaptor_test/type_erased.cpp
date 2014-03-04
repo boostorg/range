8de7e29fc711fd873b35a1679a0b68151ec0ca73
Neil Groves <neilgroves@googlemail.com>
2014-03-04 11:26:50 +0000

    type_erased mix of values test separated.

--- test/adaptor_test/type_erased.cpp
+++ test/adaptor_test/type_erased.cpp
@@ -23,158 +23,142 @@
 
 namespace boost_range_adaptor_type_erased_test
 {
-
-    void test_type_erased()
+    namespace
     {
-        test_driver< std::list<int> >();
-        test_driver< std::vector<int> >();
 
-        test_driver< std::list<MockType> >();
-        test_driver< std::vector<MockType> >();
-    }
+void test_type_erased()
+{
+    test_driver< std::list<int> >();
+    test_driver< std::vector<int> >();
 
-    void test_type_erased_single_pass()
-    {
-        test_type_erased_exercise_buffer_types< std::list<int>, boost::single_pass_traversal_tag >();
-        test_type_erased_exercise_buffer_types< std::deque<int>, boost::single_pass_traversal_tag >();
-        test_type_erased_exercise_buffer_types< std::vector<int>, boost::single_pass_traversal_tag >();
-
-        test_type_erased_exercise_buffer_types< std::list<MockType>, boost::single_pass_traversal_tag >();
-        test_type_erased_exercise_buffer_types< std::deque<MockType>, boost::single_pass_traversal_tag >();
-        test_type_erased_exercise_buffer_types< std::vector<MockType>, boost::single_pass_traversal_tag >();
-    }
-
-    void test_type_erased_forward()
-    {
-        test_type_erased_exercise_buffer_types< std::list<int>, boost::forward_traversal_tag >();
-        test_type_erased_exercise_buffer_types< std::deque<int>, boost::forward_traversal_tag >();
-        test_type_erased_exercise_buffer_types< std::vector<int>, boost::forward_traversal_tag >();
-
-        test_type_erased_exercise_buffer_types< std::list<MockType>, boost::forward_traversal_tag >();
-        test_type_erased_exercise_buffer_types< std::deque<MockType>, boost::forward_traversal_tag >();
-        test_type_erased_exercise_buffer_types< std::vector<MockType>, boost::forward_traversal_tag >();
-    }
-
-    void test_type_erased_bidirectional()
-    {
-        test_type_erased_exercise_buffer_types< std::list<int>, boost::bidirectional_traversal_tag >();
-        test_type_erased_exercise_buffer_types< std::deque<int>, boost::bidirectional_traversal_tag >();
-        test_type_erased_exercise_buffer_types< std::vector<int>, boost::bidirectional_traversal_tag >();
-
-        test_type_erased_exercise_buffer_types< std::list<MockType>, boost::bidirectional_traversal_tag >();
-        test_type_erased_exercise_buffer_types< std::deque<MockType>, boost::bidirectional_traversal_tag >();
-        test_type_erased_exercise_buffer_types< std::vector<MockType>, boost::bidirectional_traversal_tag >();
-    }
-
-    void test_type_erased_random_access()
-    {
-        test_type_erased_exercise_buffer_types< std::deque<int>, boost::random_access_traversal_tag >();
-        test_type_erased_exercise_buffer_types< std::vector<int>, boost::random_access_traversal_tag >();
-
-        test_type_erased_exercise_buffer_types< std::deque<MockType>, boost::random_access_traversal_tag >();
-        test_type_erased_exercise_buffer_types< std::vector<MockType>, boost::random_access_traversal_tag >();
-    }
-
-    void test_type_erased_multiple_different_template_parameter_conversion()
-    {
-        typedef boost::any_range<
-                    int
-                  , boost::random_access_traversal_tag
-                  , int&
-                  , std::ptrdiff_t
-        > source_range_type;
-
-        typedef boost::any_range<
-                    int
-                  , boost::single_pass_traversal_tag
-                  , const int&
-                  , std::ptrdiff_t
-        > target_range_type;
-
-        source_range_type source;
-
-        // Converting via construction
-        target_range_type t1(source);
-
-        // Converting via assignment
-        target_range_type t2;
-        t2 = source;
-
-        // Converting via construction to a type with a reference type
-        // that is a value
-        typedef boost::any_range<
-                    int
-                  , boost::single_pass_traversal_tag
-                  , int
-                  , std::ptrdiff_t
-                > target_range2_type;
-
-        target_range2_type t3(source);
-        target_range2_type t4;
-        t4 = source;
-    }
-
-    template<
-        class Traversal
-      , class ValueType
-      , class SourceValueType
-      , class SourceReference
-      , class TargetValueType
-      , class TargetReference
-    >
-    void test_type_erased_mix_values_impl()
-    {
-        typedef std::vector< ValueType > Container;
-
-        typedef typename boost::any_range_type_generator<
-            Container
-          , SourceValueType
-          , Traversal
-          , SourceReference
-        >::type source_type;
-
-        typedef typename boost::any_range_type_generator<
-            Container
-          , TargetValueType
-          , Traversal
-          , TargetReference
-        >::type target_type;
-
-        Container test_data;
-        for (int i = 0; i < 10; ++i)
-            test_data.push_back(i);
-
-        const source_type source_data(test_data);
-        target_type t1(source_data);
-        BOOST_CHECK_EQUAL_COLLECTIONS( source_data.begin(), source_data.end(),
-                                       t1.begin(), t1.end() );
-
-        target_type t2;
-        t2 = source_data;
-        BOOST_CHECK_EQUAL_COLLECTIONS( source_data.begin(), source_data.end(),
-                                       t2.begin(), t2.end() );
-    }
-
-    template<class Traversal>
-    void test_type_erased_mix_values_driver()
-    {
-        test_type_erased_mix_values_impl<
-            Traversal,
-            MockType,
-            MockType2, const MockType&,
-            MockType, const MockType&
-        >();
-    }
-
-    void test_type_erased_mix_values()
-    {
-        test_type_erased_mix_values_driver< boost::single_pass_traversal_tag >();
-        test_type_erased_mix_values_driver< boost::forward_traversal_tag >();
-        test_type_erased_mix_values_driver< boost::bidirectional_traversal_tag >();
-        test_type_erased_mix_values_driver< boost::random_access_traversal_tag >();
-    }
-
+    test_driver< std::list<MockType> >();
+    test_driver< std::vector<MockType> >();
 }
+
+void test_type_erased_single_pass()
+{
+    test_type_erased_exercise_buffer_types< std::list<int>, boost::single_pass_traversal_tag >();
+    test_type_erased_exercise_buffer_types< std::deque<int>, boost::single_pass_traversal_tag >();
+    test_type_erased_exercise_buffer_types< std::vector<int>, boost::single_pass_traversal_tag >();
+
+    test_type_erased_exercise_buffer_types< std::list<MockType>, boost::single_pass_traversal_tag >();
+    test_type_erased_exercise_buffer_types< std::deque<MockType>, boost::single_pass_traversal_tag >();
+    test_type_erased_exercise_buffer_types< std::vector<MockType>, boost::single_pass_traversal_tag >();
+}
+
+void test_type_erased_forward()
+{
+    test_type_erased_exercise_buffer_types< std::list<int>, boost::forward_traversal_tag >();
+    test_type_erased_exercise_buffer_types< std::deque<int>, boost::forward_traversal_tag >();
+    test_type_erased_exercise_buffer_types< std::vector<int>, boost::forward_traversal_tag >();
+
+    test_type_erased_exercise_buffer_types< std::list<MockType>, boost::forward_traversal_tag >();
+    test_type_erased_exercise_buffer_types< std::deque<MockType>, boost::forward_traversal_tag >();
+    test_type_erased_exercise_buffer_types< std::vector<MockType>, boost::forward_traversal_tag >();
+}
+
+void test_type_erased_bidirectional()
+{
+    test_type_erased_exercise_buffer_types< std::list<int>, boost::bidirectional_traversal_tag >();
+    test_type_erased_exercise_buffer_types< std::deque<int>, boost::bidirectional_traversal_tag >();
+    test_type_erased_exercise_buffer_types< std::vector<int>, boost::bidirectional_traversal_tag >();
+
+    test_type_erased_exercise_buffer_types< std::list<MockType>, boost::bidirectional_traversal_tag >();
+    test_type_erased_exercise_buffer_types< std::deque<MockType>, boost::bidirectional_traversal_tag >();
+    test_type_erased_exercise_buffer_types< std::vector<MockType>, boost::bidirectional_traversal_tag >();
+}
+
+void test_type_erased_random_access()
+{
+    test_type_erased_exercise_buffer_types< std::deque<int>, boost::random_access_traversal_tag >();
+    test_type_erased_exercise_buffer_types< std::vector<int>, boost::random_access_traversal_tag >();
+
+    test_type_erased_exercise_buffer_types< std::deque<MockType>, boost::random_access_traversal_tag >();
+    test_type_erased_exercise_buffer_types< std::vector<MockType>, boost::random_access_traversal_tag >();
+}
+
+void test_type_erased_multiple_different_template_parameter_conversion()
+{
+    typedef boost::any_range<
+                int
+              , boost::random_access_traversal_tag
+              , int&
+              , std::ptrdiff_t
+    > source_range_type;
+
+    typedef boost::any_range<
+                int
+              , boost::single_pass_traversal_tag
+              , const int&
+              , std::ptrdiff_t
+    > target_range_type;
+
+    source_range_type source;
+
+    // Converting via construction
+    target_range_type t1(source);
+
+    // Converting via assignment
+    target_range_type t2;
+    t2 = source;
+
+    // Converting via construction to a type with a reference type
+    // that is a value
+    typedef boost::any_range<
+                int
+              , boost::single_pass_traversal_tag
+              , int
+              , std::ptrdiff_t
+            > target_range2_type;
+
+    target_range2_type t3(source);
+    target_range2_type t4;
+    t4 = source;
+}
+
+template<
+    class Traversal
+  , class ValueType
+  , class SourceValueType
+  , class SourceReference
+  , class TargetValueType
+  , class TargetReference
+>
+void test_type_erased_mix_values_impl()
+{
+    typedef std::vector< ValueType > Container;
+
+    typedef typename boost::any_range_type_generator<
+        Container
+      , SourceValueType
+      , Traversal
+      , SourceReference
+    >::type source_type;
+
+    typedef typename boost::any_range_type_generator<
+        Container
+      , TargetValueType
+      , Traversal
+      , TargetReference
+    >::type target_type;
+
+    Container test_data;
+    for (int i = 0; i < 10; ++i)
+        test_data.push_back(i);
+
+    const source_type source_data(test_data);
+    target_type t1(source_data);
+    BOOST_CHECK_EQUAL_COLLECTIONS( source_data.begin(), source_data.end(),
+                                   t1.begin(), t1.end() );
+
+    target_type t2;
+    t2 = source_data;
+    BOOST_CHECK_EQUAL_COLLECTIONS( source_data.begin(), source_data.end(),
+                                   t2.begin(), t2.end() );
+}
+
+    } // anonymous namespace
+} // namespace boost_range_adaptor_type_erased_test
 
 boost::unit_test::test_suite*
 init_unit_test_suite(int argc, char* argv[])
@@ -188,7 +172,6 @@ init_unit_test_suite(int argc, char* argv[])
     test->add( BOOST_TEST_CASE( &boost_range_adaptor_type_erased_test::test_type_erased_bidirectional ) );
     test->add( BOOST_TEST_CASE( &boost_range_adaptor_type_erased_test::test_type_erased_random_access ) );
     test->add( BOOST_TEST_CASE( &boost_range_adaptor_type_erased_test::test_type_erased_multiple_different_template_parameter_conversion ) );
-    test->add( BOOST_TEST_CASE( &boost_range_adaptor_type_erased_test::test_type_erased_mix_values ) );
 
     return test;
 }
