379fbc51e3f182609a27d26800c91adab77c05df
Neil Groves <neilgroves@googlemail.com>
2014-03-04 11:46:57 +0000

    type_erased unit test separated bidirectional traversal.

--- test/adaptor_test/type_erased.cpp
+++ test/adaptor_test/type_erased.cpp
@@ -10,15 +10,13 @@
 
 #include <boost/test/unit_test.hpp>
 
-#include <boost/range/algorithm_ext.hpp>
-#include <boost/range/algorithm/fill.hpp>
 #include <boost/range/value_type.hpp>
 #include <boost/array.hpp>
 #include <boost/cstdint.hpp>
 
 #include <algorithm>
 #include <list>
-#include <set>
+#include <deque>
 #include <vector>
 
 namespace boost_range_adaptor_type_erased_test
@@ -46,17 +44,6 @@ void test_type_erased_forward()
     test_type_erased_exercise_buffer_types< std::vector<MockType>, boost::forward_traversal_tag >();
 }
 
-void test_type_erased_bidirectional()
-{
-    test_type_erased_exercise_buffer_types< std::list<int>, boost::bidirectional_traversal_tag >();
-    test_type_erased_exercise_buffer_types< std::deque<int>, boost::bidirectional_traversal_tag >();
-    test_type_erased_exercise_buffer_types< std::vector<int>, boost::bidirectional_traversal_tag >();
-
-    test_type_erased_exercise_buffer_types< std::list<MockType>, boost::bidirectional_traversal_tag >();
-    test_type_erased_exercise_buffer_types< std::deque<MockType>, boost::bidirectional_traversal_tag >();
-    test_type_erased_exercise_buffer_types< std::vector<MockType>, boost::bidirectional_traversal_tag >();
-}
-
 void test_type_erased_random_access()
 {
     test_type_erased_exercise_buffer_types< std::deque<int>, boost::random_access_traversal_tag >();
@@ -77,7 +64,6 @@ init_unit_test_suite(int argc, char* argv[])
 
     test->add( BOOST_TEST_CASE( &boost_range_adaptor_type_erased_test::test_type_erased ) );
     test->add( BOOST_TEST_CASE( &boost_range_adaptor_type_erased_test::test_type_erased_forward ) );
-    test->add( BOOST_TEST_CASE( &boost_range_adaptor_type_erased_test::test_type_erased_bidirectional ) );
     test->add( BOOST_TEST_CASE( &boost_range_adaptor_type_erased_test::test_type_erased_random_access ) );
 
     return test;
