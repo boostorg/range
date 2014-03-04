620653545317506abfb15b4c7bb72f6a0004925b
Neil Groves <neilgroves@googlemail.com>
2014-03-04 12:02:28 +0000

    type_erased random_access adapter separated.

--- test/adaptor_test/type_erased_random_access.cpp
+++ test/adaptor_test/type_erased_random_access.cpp
@@ -1,6 +1,6 @@
 // Boost.Range library
 //
-//  Copyright Neil Groves 2010. Use, modification and
+//  Copyright Neil Groves 2014. Use, modification and
 //  distribution is subject to the Boost Software License, Version
 //  1.0. (See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt)
@@ -15,7 +15,7 @@
 #include <boost/cstdint.hpp>
 
 #include <algorithm>
-#include <list>
+#include <deque>
 #include <vector>
 
 namespace boost_range_adaptor_type_erased_test
@@ -23,13 +23,19 @@ namespace boost_range_adaptor_type_erased_test
     namespace
     {
 
-void test_type_erased()
+void test_random_access()
 {
-    test_driver< std::list<int> >();
-    test_driver< std::vector<int> >();
+    test_type_erased_exercise_buffer_types<
+            std::deque<int>, boost::random_access_traversal_tag >();
 
-    test_driver< std::list<MockType> >();
-    test_driver< std::vector<MockType> >();
+    test_type_erased_exercise_buffer_types<
+            std::vector<int>, boost::random_access_traversal_tag >();
+
+    test_type_erased_exercise_buffer_types<
+            std::deque<MockType>, boost::random_access_traversal_tag >();
+
+    test_type_erased_exercise_buffer_types<
+            std::vector<MockType>, boost::random_access_traversal_tag >();
 }
 
     } // anonymous namespace
@@ -39,10 +45,10 @@ boost::unit_test::test_suite*
 init_unit_test_suite(int argc, char* argv[])
 {
     boost::unit_test::test_suite* test =
-        BOOST_TEST_SUITE("RangeTestSuite.adaptor.type_erased");
+        BOOST_TEST_SUITE("RangeTestSuite.adaptor.type_erased_random_access");
 
     test->add(BOOST_TEST_CASE(
-                &boost_range_adaptor_type_erased_test::test_type_erased));
+            &boost_range_adaptor_type_erased_test::test_random_access));
 
     return test;
 }
