1965b797d8eed824a4aef6259e913b7cb2dd5c00
levinmk <mlevin@svarnetics.org>
2014-12-12 16:43:32 -0600

    fix ticket #10754
    as described in https://svn.boost.org/trac/boost/ticket/10754, postfix_increment_proxy and writable_postfix_increment_proxy were moved to boost::iterators::detail namespace

--- include/boost/range/detail/any_iterator.hpp
+++ include/boost/range/detail/any_iterator.hpp
@@ -114,6 +114,7 @@ namespace boost
         };
     } // namespace range_detail
 
+    namespace iterators {
     namespace detail
     {
         // Rationale:
@@ -245,8 +246,8 @@ namespace boost
             any_iterator_type stored_iterator;
         };
 
-
-    }
+    } //namespace detail
+    } //namespace iterators
 
     namespace range_detail
     {
