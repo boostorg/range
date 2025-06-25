f8a33bccd58b491ec20f412f2c782e8fe85a991f
Gautam Kumar <gautam0217@gmail.com>
2025-06-25 12:09:02 +0530

    Update iterator_range_core.hpp

--- include/boost/range/iterator_range_core.hpp
+++ include/boost/range/iterator_range_core.hpp
@@ -836,7 +836,7 @@ public:
             Construct a new sequence of the specified type from the elements
             in the given range
 
-            \param r Range An input range
+            \param r An input range
             \return New sequence
         */
         template< typename SeqT, typename Range >
